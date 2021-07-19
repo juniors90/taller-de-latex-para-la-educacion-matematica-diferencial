b034f20632f7d267c6fed5b6c0ceee5839ef9238
unknown <juandavid9a0@gmail.com>
2021-07-19 14:17:50 -0300

    version 0.0.4

--- braille/source/conf.py
+++ braille/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Ferreira, Juan David'
 author = 'Ferreira, Juan David'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
